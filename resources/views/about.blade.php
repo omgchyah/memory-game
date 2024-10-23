42220f034a148a3691ee7124fe82b73897d882fc
omgchyah <rossana.liendo@gmail.com>
2024-10-23 15:59:34 +0200

    Created the layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <h1 class="text-3xl font-bold underline">Hello from the about page!</h1>
+    </body>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <h1 class="font-sans antialiased dark:bg-black dark:text-white/50">
+            <h1 class="text-3xl font-bold underline">Hello from the homepage!</h1>
+    </h1>
+</x-layout>
+
